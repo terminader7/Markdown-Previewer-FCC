3b0222ef3b151053907530ca88b1992aad0b373f
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-02-16 11:31:43 -0800

    gave functionality to text editor and previewer.

--- src/components/markDownEditor.js
+++ src/components/markDownEditor.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-export const MarkDownEditor = () => {
+export const MarkDownEditor = ({ setEditorText }) => {
 
 return (
     <div id='editor-box'>
         <h3>Editor</h3>
-        <textarea id='editor'>hellowoosdfoi</textarea>
+        <textarea id='editor' onChange={(event) => setEditorText(event.target.value)}></textarea>
     </div>
 );
 }
