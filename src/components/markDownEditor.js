4a0b4761ee65546f3578d53098c8375875a78962
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-02-16 15:11:27 -0800

    editor functionality finished, styling still to do

--- src/components/markDownEditor.js
+++ src/components/markDownEditor.js
@@ -1,13 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { placeHolder } from "../constants/placeholder";
 
 export const MarkDownEditor = ({ setEditorText }) => {
+  const [defaultText, _ /*ignore setDefaultText*/] = useState(placeHolder);
+  useEffect(() => {
+    setEditorText(defaultText);
+  }, []);
   return (
     <div id="editor-box">
       <h3>Editor</h3>
       <textarea
         id="editor"
         onChange={(event) => setEditorText(event.target.value)}
-      ></textarea>
+      >
+        {defaultText}
+      </textarea>
     </div>
   );
 };
